32597b53c336522ad28ca6c8667332ba7118a5b4
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-15 20:22:58 +0300

    Update 7. Cut and Reverse.js

--- 7. Cut and Reverse.js
+++ 7. Cut and Reverse.js
@@ -1,7 +1,7 @@
 function cutAndReverse(str){
-let first = str.slice(0, str.length/2).split(``).reverse()
-let second = str.slice(str.length/2).split(``).reverse()
-
+let first = str.slice(0, str.length/2).split(``).reverse();
+let second = str.slice(str.length/2).split(``).reverse();
+   
     console.log(`${first.join(``)}\n${second.join(``)}`);
     
 }
