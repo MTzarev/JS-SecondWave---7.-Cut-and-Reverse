fd6f7b825caef4fc644ddef8962a539582f1fa81
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-15 19:00:58 +0300

    Add files via upload

---
+++ 7. Cut and Reverse.js
@@ -0,0 +1,8 @@
+function cutAndReverse(str){
+let first = str.slice(0, str.length/2).split(``).reverse()
+let second = str.slice(str.length/2).split(``).reverse()
+
+    console.log(`${first.join(``)}\n${second.join(``)}`);
+    
+}
+cutAndReverse('sihToDtnaCuoYteBIboJsihTtAdooGoSmI');
